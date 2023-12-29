7d88155452414e55eacdc614bc4438152b92366a
Lhsales <48722995+Lhsales@users.noreply.github.com>
2023-12-28 23:20:50 -0300

    Controllers e Routes
    - Utilização básica de controller;
    - Desenvolvimento de rotas.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contato/{id?}', ['uses'=>'App\Http\Controllers\ContatoController@Index']);
+Route::post('/contato', ['uses'=>'App\Http\Controllers\ContatoController@Criar']);
+Route::put('/contato', ['uses'=>'App\Http\Controllers\ContatoController@Editar']);
+
