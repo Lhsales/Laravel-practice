8ef38f9ca438be2fb99a3822c6701cab43ce00e1
Lhsales <48722995+Lhsales@users.noreply.github.com>
2023-12-31 00:08:14 -0300

    Alterado de Bootstrap para Materialize
    - Alterado design de top, footer e site para Materialize.

--- resources/views/layout/include/top.blade.php
+++ resources/views/layout/include/top.blade.php
@@ -1,18 +1,10 @@
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="#">Laravel-Practice</a>
-        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavId">
-            <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('home') }}">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('contato') }}">Contato</a>
-                </li>
-            </ul>
-        </div>
+<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo" style="padding-left: 10px">Laravel Practice</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="{{ route('contato') }}">Contatos</a></li>
+        <li><a href="{{ route('admin.cursos') }}">Cursos</a></li>
+      </ul>
     </div>
-</nav>
+  </nav>
--- resources/views/layout/site.blade.php
+++ resources/views/layout/site.blade.php
@@ -5,20 +5,31 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('titulo')</title>
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+        <link rel="stylesheet" type="text/css" href="{{ url('/css/app.css') }}" />
     </head>
     <body>
         @include('layout.include.top')
-        <div class="container-fluid text-center py-4">
-            <h1>@yield('title-content')</h1>
-        </div>
-        
-        <div class="container">
-
-            @yield('conteudo')
-
-        </div>
+        <main>
+            <div class="container-fluid text-center py-4">
+                <h1>@yield('title-content')</h1>
+            </div>
+            
+            <div class="container">
+            
+                @yield('conteudo')
+            
+            </div>
+        </main>
         @include('layout.include.footer')
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> 
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('.sidenav').sidenav();
+            });
+        </script>
     </body>
 </html>
